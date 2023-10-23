ac2ea1be1fe827d2b9ce6879335139caa28a92f3
vinicioscsoares <vinicioscsoares@gmail.com>
2023-10-23 15:02:55 -0300

    Mensagens em tempo real enviadas pela rede :)

---
+++ test.py
@@ -0,0 +1,14 @@
+import socket
+ 
+HOST = 'localhost'
+PORT = 55556
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+client.connect((HOST,PORT))
+
+mensagem = client.recv(1024)
+if mensagem == b'SALA':
+  client.send(b'fernando')
+  client.send(b'JOGOS')
+  
